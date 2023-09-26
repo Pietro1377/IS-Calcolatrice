2d4691486875249ba0b888b5d1652abde99ee9fd
FireLion137 <pietroespo8@gmail.com>
2023-09-26 15:55:42 +0200

    Rimosso codice inutile

--- src/main/java/com/is/calcolatrice/Calcolatrice.java
+++ src/main/java/com/is/calcolatrice/Calcolatrice.java
@@ -1,13 +1,6 @@
 package com.is.calcolatrice;
 
 public class Calcolatrice {
-    private final int a, b;
-
-    public Calcolatrice() {
-        this.a= 0;
-        this.b= 0;
-    }
-
     public int somma(int a, int b) {
         return a+b;
     }
